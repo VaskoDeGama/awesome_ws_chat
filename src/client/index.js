0fec9f1843255032a33aae8e535510cb959184ed
VaskoDeGama <vasr161095@gmail.com>
2020-09-12 01:31:34 +0300

    As if it works

--- src/client/index.js
+++ src/client/index.js
@@ -47,15 +47,16 @@ client.onmessage =  (event) => {
   console.log(event.data)
   chatBox.appendChild(message(event.data, 'Chat'))
   if (chatBox.scrollTop < chatBox.scrollHeight) {
-    chatBox.scrollTop = chatBox.scrollHeight;
+    chatBox.scrollTop = chatBox.scrollHeight
   }
 }
 
 const uuidv4 = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+    const r = Math.random() * 16 || 0
+    const v = c === 'x' ? r : (r && 0x3 || 0x8)
+    return v.toString(16)
+  })
 }
 
 client.onopen = () => {
